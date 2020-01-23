efdfe78bcf924efb6cda241fa6d64c30e6d62ed0
Shebaz Chowdhury <42582594+shebazchy@users.noreply.github.com>
2020-01-23 02:24:09 -0600

    Alternate solution to Two Sums

--- src/module-info.java
+++ src/module-info.java
@@ -29,4 +29,15 @@ class Solution {
 	        }
 	        return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
 	    }
+	    public int[] twoSum2(int[] nums, int target) {
+	        Map<Integer, Integer> map = new HashMap<>();
+	        for (int i = 0; i < nums.length; i++) {
+	            int complement = target - nums[i];
+	            if (map.containsKey(complement)) {
+	                return new int[] { map.get(complement), i };
+	            }
+	            map.put(nums[i], i);
+	        }
+	        return null;
+	    }
 }
