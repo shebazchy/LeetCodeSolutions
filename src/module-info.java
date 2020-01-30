34cd5ecccc7551d85526d9d8edc930b49980ba31
Shebaz Chowdhury <42582594+shebazchy@users.noreply.github.com>
2020-01-29 20:57:17 -0600

    updated leetcode solutions

--- src/module-info.java
+++ src/module-info.java
@@ -40,4 +40,38 @@ class Solution {
 	        }
 	        return null;
 	    }
+	    public ListNode reverseList(ListNode head) {
+	         ListNode prev = null;
+	    ListNode curr = head;
+	    while (curr != null) {
+	        ListNode nextTemp = curr.next;
+	        curr.next = prev;
+	        prev = curr;
+	        curr = nextTemp;
+	    }
+	    return prev;
+	    }
+	    public void setZeroes(int[][] matrix) {
+	        int R = matrix.length;
+	        int C = matrix[0].length;
+	        Set<Integer> rows = new HashSet<Integer>();
+	        Set<Integer> cols = new HashSet<Integer>();
+	        
+	        for (int i = 0; i < R; i++) {
+	          for (int j = 0; j < C; j++) {
+	            if (matrix[i][j] == 0) {
+	              rows.add(i);
+	              cols.add(j);
+	            }
+	          }
+	        }
+
+	        for (int i = 0; i < R; i++) {
+	          for (int j = 0; j < C; j++) {
+	            if (rows.contains(i) || cols.contains(j)) {
+	              matrix[i][j] = 0;
+	            }
+	          }
+	        }
+	    }
 }
